7cc05f0a362dab66ca774e0543706b6826c5b470
Irene Hofstetter <irene.hofstetter@npower.ch>
2017-01-15 12:36:14 +0100

    misc
    * added new project for testing
    * updated to .NET 4.6.2
    * updated packages
    * added loop test

---
+++ ConsoleApplication1/ch.npower.Examples.CSharp.Tests/LoopTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ch.npower.Examples.CSharp.Tests
+{
+    [TestClass]
+    public class LoopTest
+    {
+        [TestMethod]
+        public void LoopForRunTest()
+        {
+            // Arrange
+            var sut = new Loop.LoopFor();
+
+            // Act
+            var result = sut.Run();
+
+            // Assert
+            Assert.AreEqual(Loop.LoopFor.MAX_COUNT, result);
+
+        }
+    }
+}
---
+++ ConsoleApplication1/ch.npower.Examples.CSharp/Loop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch.npower.Examples.CSharp
+{
+    public class Loop
+    {
+        public class LoopFor
+        {
+            public const int MAX_COUNT = 1000;
+            public int Run()
+            {
+                int counter;
+                for(counter = 0; counter < MAX_COUNT; counter++)
+                {
+                    // tralala
+                }
+
+                return counter;
+            }
+        }
+    }
+}
